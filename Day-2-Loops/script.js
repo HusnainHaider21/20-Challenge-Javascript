d509faf2ccdede8cb57b8b7a77021ffcc5ede8ed
HusnainHaider21 <husnainhaider0304@gmail.com>
2026-01-01 14:14:03 -0800

    loops completed

---
+++ Day-2-Loops/script.js
@@ -0,0 +1,41 @@
+// Topic: Loops 
+// For Loop
+// 1.Print Numbers 1 to 10
+for (let i = 1; i <=10; i++ ){
+        console.log(i);   
+}
+
+// 2.Print Numbers 10 to 1
+for (let i = 10; i >= 1; i--){
+    console.log(i);    
+}
+
+// 3.Print Even Numbers from 1 to 20
+for (let i = 1; i <= 20; i++){
+    if (i % 2 ===0) {
+        console.log(i);
+    } 
+}
+// while Loops
+// 1.Print Numbers 1 to 10
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+// Print numbers 10 to 1
+let j = 10;
+while (j >= 1) {
+    console.log(j);
+    j--;
+}
+
+// Print Even Numbers from 1 to 20
+let k = 1;
+while (k <= 20){
+    if (k % 2 ===0) {
+        console.log(k);
+        
+    }
+    k++;
+}
